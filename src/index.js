90fee1d27de92f276ce2633c07f8e0aef481e0e2
Dafei Ning <ningdafei@gmail.com>
2018-09-30 15:12:35 -0700

    first add

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+
+function timeout(duration = 0) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => resolve(1), duration);
+	})
+}
+
+// Chain
+timeout(1000)
+.then(
+	(v) => {
+		console.log('resolved with: ' + v);
+		return timeout(2000);
+	}, 
+	(v) => {
+		console.log('rejected with: ' + v);
+		return timeout(2000);
+	})
+.then(
+	(v) => {
+		console.log('resolved with: ' + v);
+		throw new Error("hmm");
+	}, 
+	(v) => {
+		console.log('rejected with: ' + v);
+	})
+.catch((err) => {
+	console.log(err);
+}
+)
+
+
+
+
